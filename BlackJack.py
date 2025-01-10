129169d71fc17dd65713e584bbc9765e753b7374
rigellin73 <rigellin73@gmail.com>
2025-01-10 18:15:24 +0100

    simplify output and add final output with results

--- BlackJack.py
+++ BlackJack.py
@@ -6,6 +6,7 @@ deck = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"] * 4
 game_over = False
 player_cards = []
 computer_cards = []
+final_message = ""
 
 def pick_a_card():
     card = random.choice(deck)
@@ -38,6 +39,10 @@ def input_another_card():
         another_card = input("Type 'y' to get another card, type 'n' to pass: ")
     return another_card
 
+def print_result():
+    print(f"Your cards: {player_cards}, current score: {player_score}")
+    print(f"Computer's cards: {computer_cards}, current score: {computer_score}")
+
 print(logo)
 start_hand()
 player_score = calculate_score(0, player_cards[0])
@@ -45,8 +50,7 @@ player_score = calculate_score(player_score, player_cards[1])
 computer_score = calculate_score(0, computer_cards[0])
 computer_score = calculate_score(computer_score, computer_cards[1])
 
-print(f"Your cards: {player_cards}, current score: {player_score}")
-print(f"Computer cards: {computer_cards}, computer score: {computer_score}")
+print_result()
 
 while not game_over:
     need_card = input_another_card()
@@ -54,34 +58,38 @@ while not game_over:
     if need_card == 'y':
         player_cards.append(pick_a_card())
         player_score = calculate_score(player_score, player_cards[-1])
-        print(f"Your cards: {player_cards}, current score: {player_score}")
-        print(f"Computer cards: {computer_cards}, computer score: {computer_score}")
+
         if player_score > 21:
-            print("You lose!")
+            final_message = "You went over. You lose."
             game_over = True
         else:
             if computer_score < 17:
                 computer_cards.append(pick_a_card())
                 computer_score = calculate_score(computer_score, computer_cards[-1])
-                print(f"Your cards: {player_cards}, current score: {player_score}")
-                print(f"Computer cards: {computer_cards}, computer score: {computer_score}")
                 if computer_score > 21:
-                    print("You win!")
+                    final_message = "You win!"
                     game_over = True
+
+        print_result()
     else:
         while computer_score < 17:
             computer_cards.append(pick_a_card())
             computer_score = calculate_score(computer_score, computer_cards[-1])
-            print(f"Your cards: {player_cards}, current score: {player_score}")
-            print(f"Computer cards: {computer_cards}, computer score: {computer_score}")
+            print_result()
             if computer_score > 21:
-                print("You win!")
+                final_message = "You win!"
                 game_over = True
         if not game_over:
             if player_score > computer_score:
-                print("You win!")
+                final_message = "You win!"
             elif player_score == computer_score:
-                print("Draw!")
+                final_message = "Draw!"
             else:
-                print("You lose!")
+                final_message = "You lose."
             game_over = True
+
+print()
+print("Game finished")
+print(f"Your final hand: {player_cards}, final score: {player_score}")
+print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
+print(final_message)
