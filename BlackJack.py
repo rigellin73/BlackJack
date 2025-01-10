910bc8e4925855f5f22a79289cf154c03c386a01
rigellin73 <rigellin73@gmail.com>
2025-01-10 16:15:58 +0100

    add calculate_score function

--- BlackJack.py
+++ BlackJack.py
@@ -18,13 +18,43 @@ def start_hand():
     computer_cards.append(pick_a_card())
     computer_cards.append(pick_a_card())
 
+def calculate_score(current_score, new_cards):
+    for card in new_cards:
+        if card == "A":
+            if current_score + 11 > 21:
+                current_score += 1
+            else:
+                current_score += 11
+        elif card == "J" or card == "Q" or card == "K":
+            current_score += 10
+        else:
+            current_score += card
+
+    return current_score
+
+def input_another_card():
+    another_card = input("Type 'y' to get another card, type 'n' to pass: ")
+    while not (another_card == "y" or another_card == "n"):
+        print("Unexpected input")
+        another_card = input("Type 'y' to get another card, type 'n' to pass: ")
+    return another_card
+
 print(logo)
+start_hand()
+player_score = calculate_score(0, player_cards)
+computer_score = calculate_score(0, computer_cards)
 
 while not game_over:
-    start_hand()
+    need_card = input_another_card()
 
-    print(f"Your cards: {player_cards}")
-    print(f"Computer cards: {computer_cards}")
-    print(deck)
+    print(f"Your cards: {player_cards}, current score: {player_score}")
+    print(f"Computer cards: {computer_cards}, computer score: {computer_score}")
+    #print(deck)
 
-    game_over = True
+    if player_score > 21:
+        print("You lose!")
+        game_over = True
+
+    if computer_score > 21:
+        print("You win!")
+        game_over = True
