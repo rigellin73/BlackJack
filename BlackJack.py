60c221649ad5bf04b31ba8a97eb2a7acf82502ad
rigellin73 <rigellin73@gmail.com>
2025-01-10 17:54:17 +0100

    add win conditions

--- BlackJack.py
+++ BlackJack.py
@@ -18,17 +18,16 @@ def start_hand():
     computer_cards.append(pick_a_card())
     computer_cards.append(pick_a_card())
 
-def calculate_score(current_score, new_cards):
-    for card in new_cards:
-        if card == "A":
-            if current_score + 11 > 21:
-                current_score += 1
-            else:
-                current_score += 11
-        elif card == "J" or card == "Q" or card == "K":
-            current_score += 10
+def calculate_score(current_score, card):
+    if card == "A":
+        if current_score + 11 > 21:
+            current_score += 1
         else:
-            current_score += card
+            current_score += 11
+    elif card == "J" or card == "Q" or card == "K":
+        current_score += 10
+    else:
+        current_score += card
 
     return current_score
 
@@ -41,20 +40,48 @@ def input_another_card():
 
 print(logo)
 start_hand()
-player_score = calculate_score(0, player_cards)
-computer_score = calculate_score(0, computer_cards)
+player_score = calculate_score(0, player_cards[0])
+player_score = calculate_score(player_score, player_cards[1])
+computer_score = calculate_score(0, computer_cards[0])
+computer_score = calculate_score(computer_score, computer_cards[1])
+
+print(f"Your cards: {player_cards}, current score: {player_score}")
+print(f"Computer cards: {computer_cards}, computer score: {computer_score}")
 
 while not game_over:
     need_card = input_another_card()
 
-    print(f"Your cards: {player_cards}, current score: {player_score}")
-    print(f"Computer cards: {computer_cards}, computer score: {computer_score}")
-    #print(deck)
-
-    if player_score > 21:
-        print("You lose!")
-        game_over = True
-
-    if computer_score > 21:
-        print("You win!")
-        game_over = True
+    if need_card == 'y':
+        player_cards.append(pick_a_card())
+        player_score = calculate_score(player_score, player_cards[-1])
+        print(f"Your cards: {player_cards}, current score: {player_score}")
+        print(f"Computer cards: {computer_cards}, computer score: {computer_score}")
+        if player_score > 21:
+            print("You lose!")
+            game_over = True
+        else:
+            if computer_score < 17:
+                computer_cards.append(pick_a_card())
+                computer_score = calculate_score(computer_score, computer_cards[-1])
+                print(f"Your cards: {player_cards}, current score: {player_score}")
+                print(f"Computer cards: {computer_cards}, computer score: {computer_score}")
+                if computer_score > 21:
+                    print("You win!")
+                    game_over = True
+    else:
+        while computer_score < 17:
+            computer_cards.append(pick_a_card())
+            computer_score = calculate_score(computer_score, computer_cards[-1])
+            print(f"Your cards: {player_cards}, current score: {player_score}")
+            print(f"Computer cards: {computer_cards}, computer score: {computer_score}")
+            if computer_score > 21:
+                print("You win!")
+                game_over = True
+        if not game_over:
+            if player_score > computer_score:
+                print("You win!")
+            elif player_score == computer_score:
+                print("Draw!")
+            else:
+                print("You lose!")
+            game_over = True
