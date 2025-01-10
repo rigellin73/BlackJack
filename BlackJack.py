11e42a9eb869310641d310e258a025bf7ed7e31a
rigellin73 <rigellin73@gmail.com>
2025-01-10 15:52:17 +0100

    create start hand

--- BlackJack.py
+++ BlackJack.py
@@ -1,2 +1,30 @@
+import random
+
 from ascii_art import logo
 
+deck = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"] * 4
+game_over = False
+player_cards = []
+computer_cards = []
+
+def pick_a_card():
+    card = random.choice(deck)
+    deck.remove(card)
+    return card
+
+def start_hand():
+    player_cards.append(pick_a_card())
+    player_cards.append(pick_a_card())
+    computer_cards.append(pick_a_card())
+    computer_cards.append(pick_a_card())
+
+print(logo)
+
+while not game_over:
+    start_hand()
+
+    print(f"Your cards: {player_cards}")
+    print(f"Computer cards: {computer_cards}")
+    print(deck)
+
+    game_over = True
